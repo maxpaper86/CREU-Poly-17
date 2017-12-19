e348ff83219193a1aaf06ddf95fe4355ec9fc60d
vcedgar <vatriciaedgar@gmail.com>
2017-12-18 20:38:08 -0700

    Create Link.java

---
+++ java/Link.java
@@ -0,0 +1,18 @@
+
+public class Link implements Comparable{
+	public String link;
+	public int priority;
+	
+	public Link (String l, int p) {
+		link = l;
+		priority = p;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if(priority == ((Link)o).priority) return 0;
+		else if(priority < ((Link)o).priority) return -1;
+		else return 1;
+	}
+
+}
